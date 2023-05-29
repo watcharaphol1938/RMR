8458307c3d8ddeb396e28dd59093aae5ba97b324
watcharaphol chareerat <jeanswc1938@gmail.com>
2023-05-29 21:57:32 +0700

    update frontend 3 - close #6

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-    <DashBoard />
+    <DashBoard class="dashboard"></DashBoard>
+    <TableValue></TableValue>
   </div>
 </template>
 
 <script>
 import DashBoard from './components/DashBoard.vue';
+import TableValue from './components/TableValue.vue';
 export default {
   name: 'App',
   components: {
-    DashBoard
+    DashBoard, TableValue
   }
 }
 </script>
@@ -23,4 +25,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 } */
+.dashboard {
+  margin-bottom: 30px;
+}
 </style>
---
+++ frontend/src/components/TableValue.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="container">1</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TableValue",
+  
+};
+</script>
+
+<style>
+</style>
