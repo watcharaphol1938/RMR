037ce2229d202334260dcd8222948db41dc3f7b3
watcharaphol chareerat <jeanswc1938@gmail.com>
2023-06-01 10:37:45 +0700

    update table - close #11

--- frontend/src/components/DashBoard.vue
+++ frontend/src/components/DashBoard.vue
@@ -241,7 +241,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .factory {
   font-size: 30px;
   font-weight: bolder;
--- frontend/src/components/TableValue.vue
+++ frontend/src/components/TableValue.vue
@@ -2,10 +2,10 @@
   <div>
     <table>
       <tr>
-        <th colspan="2">Mar'23</th>
+        <th colspan="2" class="first-column">Mar'23</th>
         <th colspan="4">MAR'23</th>
-        <th colspan="4">Accumulate(Mar'23)</th>
-        <th colspan="4">APR'23</th>
+        <th colspan="4">Accumulate (Mar'23)</th>
+        <th colspan="4" class="fourth-column">APR'23</th>
       </tr>
       <tr>
         <th>KPI</th>
@@ -18,10 +18,10 @@
         <th>Actual</th>
         <th>Diff.</th>
         <th>Judge</th>
-        <th>Target</th>
-        <th>Actual</th>
-        <th>Diff.</th>
-        <th>Judge</th>
+        <th class="fourth-column">Target</th>
+        <th class="fourth-column">Actual</th>
+        <th class="fourth-column">Diff.</th>
+        <th class="fourth-column">Judge</th>
       </tr>
       <tr>
         <th colspan="2">Overall DNKH</th>
@@ -33,10 +33,10 @@
         <td>91.3%</td>
         <td>0.2%</td>
         <td>0</td>
-        <td>91.1%</td>
-        <td>91.9%</td>
-        <td>0.8%</td>
-        <td>0</td>
+        <td class="fourth-column">91.1%</td>
+        <td class="fourth-column">91.9%</td>
+        <td class="fourth-column">0.8%</td>
+        <td class="fourth-column">0</td>
       </tr>
       <tr>
         <th rowspan="2">OR/Line</th>
@@ -49,10 +49,10 @@
         <td>91.5%</td>
         <td>0.4%</td>
         <td>0</td>
-        <td>91.1%</td>
-        <td>89.5%</td>
-        <td>-1.6%</td>
-        <td>X</td>
+        <td class="fourth-column">91.1%</td>
+        <td class="fourth-column">89.5%</td>
+        <td class="fourth-column">-1.6%</td>
+        <td class="fourth-column">X</td>
       </tr>
       <tr>
         <th>Radiator line #6</th>
@@ -64,10 +64,10 @@
         <td>91.8%</td>
         <td>0.7%</td>
         <td>0</td>
-        <td>91.1%</td>
-        <td>94.2%</td>
-        <td>3.1%</td>
-        <td>0</td>
+        <td class="fourth-column">91.1%</td>
+        <td class="fourth-column">94.2%</td>
+        <td class="fourth-column">3.1%</td>
+        <td class="fourth-column">0</td>
       </tr>
     </table>
   </div>
@@ -86,12 +86,18 @@ div {
   font-family: Arial;
 }
 table, th, td {
-  width: 800px;
-  margin: auto;
   font-size: 15px;
-  padding: 2px;
   text-align: center;
   border: 1px solid black;
   border-collapse: collapse;
+}
+th {
+  padding-right: 40px;
+}
+.first-column {
+  background-color: #fff9c4;
+}
+.fourth-column {
+  background-color: #e1f5fe;
 }
 </style>
