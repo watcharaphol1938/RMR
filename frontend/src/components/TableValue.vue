ca68a8487862d4e23b43e885186a5b727c4af814
watcharaphol <jeanswc1938@gmail.com>
2023-06-14 16:22:46 +0700

    update datatable - close #26

--- frontend/src/components/TableValue.vue
+++ frontend/src/components/TableValue.vue
@@ -1,129 +1,116 @@
 <template>
   <div>
-    <div class="grid">
-      <table>
-        <tr>
-          <th colspan="2" class="first-column">Mar'23</th>
-          <th colspan="4">MAR'23</th>
-          <th colspan="4">Accumulate (Mar'23)</th>
-          <th colspan="4" class="fourth-column">APR'23</th>
-        </tr>
-        <tr>
-          <th>KPI</th>
-          <th>Line</th>
-          <th>Target</th>
-          <th>Actual</th>
-          <th>Diff.</th>
-          <th>Judge</th>
-          <th>Target</th>
-          <th>Actual</th>
-          <th>Diff.</th>
-          <th>Judge</th>
-          <th class="fourth-column">Target</th>
-          <th class="fourth-column">Actual</th>
-          <th class="fourth-column">Diff.</th>
-          <th class="fourth-column">Judge</th>
-        </tr>
-        <tr>
-          <th colspan="2">Overall DNKH</th>
-          <td>91.1%</td>
-          <td>91.2%</td>
-          <td>0.1%</td>
-          <td>O</td>
-          <td>91.1%</td>
-          <td>91.3%</td>
-          <td>0.2%</td>
-          <td>O</td>
-          <td class="fourth-column">
-            <div class="first-row-value">91.1%</div>
-          </td>
-          <td class="fourth-column">
-            <div class="first-row-value">91.9%</div>
-          </td>
-          <td class="fourth-column">
-            <div class="first-row-cal">0.8%</div>
-          </td>
-          <td class="fourth-column">
-            <div class="first-row-result">O</div>
-          </td>
-        </tr>
-        <tr>
-          <th rowspan="2">OR/Line</th>
-          <th>Magneto assy. Line#2</th>
-          <td>91.1%</td>
-          <td>91.6%</td>
-          <td>0.5%</td>
-          <td>O</td>
-          <td>91.1%</td>
-          <td>91.5%</td>
-          <td>0.4%</td>
-          <td>O</td>
-          <td class="fourth-column">
-            <div class="second-row-value">91.1%</div>
-          </td>
-          <td class="fourth-column">
-            <div class="second-row-value">89.5%</div>
-          </td>
-          <td class="fourth-column">
-            <div class="second-row-cal">-1.6%</div>
-          </td>
-          <td class="fourth-column">
-            <div class="second-row-result">X</div>
-          </td>
-        </tr>
-        <tr>
-          <th>Radiator line #6</th>
-          <td>
-            91.1%
-          </td>
-          <td>
-            92.7%
-          </td>
-          <td>
-            1.6%
-          </td>
-          <td>
-            O
-          </td>
-          <td>
-            91.1%
-          </td>
-          <td>
-            91.8%
-          </td>
-          <td>
-            0.7%
-          </td>
-          <td>
-            O
-          </td>
-          <td class="fourth-column">
-            <div class="third-row-value">91.1%</div>
-          </td>
-          <td class="fourth-column">
-            <div class="third-row-value">94.2%</div>
-          </td>
-          <td class="fourth-column">
-            <div class="third-row-cal">3.1%</div>
-          </td>
-          <td class="fourth-column">
-            <div class="third-row-result">O</div>
-          </td>
-        </tr>
-      </table>
+    <div class="container">
+      <div class="grid">
+        <table>
+          <tr>
+            <th colspan="2" class="first-column">Mar'23</th>
+            <th colspan="4">MAR'23</th>
+            <th colspan="4">Accumulate (Mar'23)</th>
+            <th colspan="4" class="fourth-column">APR'23</th>
+          </tr>
+          <tr>
+            <th>KPI</th>
+            <th>Line</th>
+            <th>Target</th>
+            <th>Actual</th>
+            <th>Diff.</th>
+            <th>Judge</th>
+            <th>Target</th>
+            <th>Actual</th>
+            <th>Diff.</th>
+            <th>Judge</th>
+            <th class="fourth-column">Target</th>
+            <th class="fourth-column">Actual</th>
+            <th class="fourth-column">Diff.</th>
+            <th class="fourth-column">Judge</th>
+          </tr>
+          <tr>
+            <th colspan="2">Overall DNKH</th>
+            <td>91.1%</td>
+            <td>91.2%</td>
+            <td>0.1%</td>
+            <td>O</td>
+            <td>91.1%</td>
+            <td>91.3%</td>
+            <td>0.2%</td>
+            <td>O</td>
+            <td class="fourth-column">
+              <div class="first-row-value">91.1%</div>
+            </td>
+            <td class="fourth-column">
+              <div class="first-row-value">91.9%</div>
+            </td>
+            <td class="fourth-column">
+              <div class="first-row-cal">0.8%</div>
+            </td>
+            <td class="fourth-column">
+              <div class="first-row-result">O</div>
+            </td>
+          </tr>
+          <tr>
+            <th rowspan="2">OR/Line</th>
+            <th>Magneto assy. Line#2</th>
+            <td>91.1%</td>
+            <td>91.6%</td>
+            <td>0.5%</td>
+            <td>O</td>
+            <td>91.1%</td>
+            <td>91.5%</td>
+            <td>0.4%</td>
+            <td>O</td>
+            <td class="fourth-column">
+              <div class="second-row-value">91.1%</div>
+            </td>
+            <td class="fourth-column">
+              <div class="second-row-value">89.5%</div>
+            </td>
+            <td class="fourth-column">
+              <div class="second-row-cal">-1.6%</div>
+            </td>
+            <td class="fourth-column">
+              <div class="second-row-result">X</div>
+            </td>
+          </tr>
+          <tr>
+            <th>Radiator line #6</th>
+            <td>91.1%</td>
+            <td>92.7%</td>
+            <td>1.6%</td>
+            <td>O</td>
+            <td>91.1%</td>
+            <td>91.8%</td>
+            <td>0.7%</td>
+            <td>O</td>
+            <td class="fourth-column">
+              <div class="third-row-value">91.1%</div>
+            </td>
+            <td class="fourth-column">
+              <div class="third-row-value">94.2%</div>
+            </td>
+            <td class="fourth-column">
+              <div class="third-row-cal">3.1%</div>
+            </td>
+            <td class="fourth-column">
+              <div class="third-row-result">O</div>
+            </td>
+          </tr>
+        </table>
+      </div>
     </div>
+
     <!-- <div v-for="data in myData" :key="data">{{ data }}</div> -->
   </div>
 </template>
 
 <script>
-import { result } from '../mocks/datasourceTable';
+import { result } from "../mocks/datasourceTable";
 export default {
   name: "TableValue",
   data() {
     return {
-      myData: result
-    }
+      myData: result,
+    };
   },
   methods: {
     // processline() {
@@ -137,11 +124,16 @@ export default {
   },
   mounted() {
     // this.processline()
-  }
+  },
 };
 </script>
 
 <style scoped>
+.container {
+  margin-top: 5px;
+  background-color: white;
+  /* border: 2px solid black; */
+}
 .grid {
   display: grid;
   grid-template-columns: 1fr;
--- frontend/src/mocks/datasourceTable.js
+++ frontend/src/mocks/datasourceTable.js
@@ -1,23 +1,74 @@
-const result = [
+const resultdata = [
+  {
+    overall: "DNKH",
+    lastmonth: [
+      {
+        target: 91.1,
+      },
+      {
+        actual: 91.2,
+      },
+      {
+        diff: 0.1,
+      },
+    ],
+    accumulatelastmonth: [
+      {
+        target: 91.1,
+      },
+      {
+        actual: 91.3,
+      },
+      {
+        diff: 0.2,
+      },
+    ],
+    currentmonth: [
+      {
+        target: 91.1,
+      },
+      {
+        actual: 91.9,
+      },
+      {
+        diff: 0.8,
+      },
+    ]
+  },
   {
     lineid: 1,
     linename: "Magnato assy. Line#2",
     lastmonth: [
       {
         target: 91.1,
+      },
+      {
         actual: 91.2,
+      },
+      {
+        diff: 0.5,
       },
     ],
     accumulatelastmonth: [
       {
         target: 91.1,
+      },
+      {
         actual: 91.3,
+      },
+      {
+        diff: 0.4,
       },
     ],
     currentmonth: [
       {
         target: 91.1,
+      },
+      {
         actual: 91.9,
+      },
+      {
+        diff: -1.6,
       },
     ],
   },
@@ -27,24 +78,39 @@ const result = [
     lastmonth: [
       {
         target: 91.1,
+      },
+      {
         actual: 92.7,
+      },
+      {
+        diff: 1.6,
       },
     ],
     accumulatelastmonth: [
       {
         target: 91.1,
+      },
+      {
         actual: 91.5,
+      },
+      {
+        diff: 0.7,
       },
     ],
     currentmonth: [
       {
         target: 91.1,
+      },
+      {
         actual: 89.5,
+      },
+      {
+        diff: 3.1,
       },
     ],
   },
 ];
 
 module.exports = {
-  result
+  resultdata
 };
