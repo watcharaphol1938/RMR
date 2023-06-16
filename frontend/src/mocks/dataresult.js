d0a21b86b4f9970a07c9215f74db8eaa78e49127
watcharaphol <jeanswc1938@gmail.com>
2023-06-16 16:07:36 +0700

    update header-result - close#28

--- frontend/src/mocks/dataresult.js
+++ frontend/src/mocks/dataresult.js
@@ -1,19 +1,8 @@
 const overall = [
     {
-        // green
-        or: 95.7,
-        target: 91.1,
+        or: 92.3,
+        target: 90,
     },
-    {
-        // yellow
-        or: 86.8,
-        target: 92.3,
-    },
-    {
-        // red
-        or: 80.5,
-        target: 90.9,
-    }
 ];
 module.exports = {
     overall
